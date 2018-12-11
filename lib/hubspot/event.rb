6c6e849e2b41b46928e7fe02b9602a1c519f5551
Matt Beedle <me@mattbeedle.name>
2018-12-11 16:16:15 +0700

    Get events

--- lib/hubspot/event.rb
+++ lib/hubspot/event.rb
@@ -7,6 +7,7 @@ module Hubspot
   # {https://developers.hubspot.com/docs/methods/enterprise_events/http_api}
   #
   class Event
+    GET_EVENTS_PATH = '/reports/v2/events'
     POST_EVENT_PATH = '/v1/event'
 
     class << self
@@ -16,6 +17,23 @@ module Hubspot
 
         Hubspot::EventConnection.trigger(POST_EVENT_PATH, options).success?
       end
+
+      def all(opts = {})
+        response = Hubspot::Connection.get_json(GET_EVENTS_PATH, opts)
+        response.map { |c| new(c) }
+      end
     end
+
+    def initialize(response_hash)
+      @properties = response_hash
+      @name = response_hash['name']
+      @id = response_hash['id']
+      @status = response_hash['status']
+    end
+
+    attr_reader :properties,
+      :name,
+      :id,
+      :status
   end
 end
